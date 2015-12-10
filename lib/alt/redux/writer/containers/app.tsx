066a2aa3d24bb2e83607bd81fd570bd9840c37a6
mmmpa <mmmpa.mmmpa@gmail.com>
2015-12-11 04:37:17 +0900

    added material ui

--- lib/alt/redux/writer/containers/app.tsx
+++ lib/alt/redux/writer/containers/app.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import { Component, PropTypes } from 'react'
+const RaisedButton = require('material-ui/lib/raised-button');
 
 export default class App extends Component {
   render() {
     return (
-      <div>test</div>
+      <div>
+        <RaisedButton label="Default" />
+      </div>
     )
   }
 }
